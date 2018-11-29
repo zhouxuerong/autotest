9c96db6c190868b921fac70ea627654418d1f52b
zhouxuerong <1343932126@qq.com>
2018-11-29 09:00:48 +0800

    add report

--- autotest/apitest/apiauto_testcase3.py
+++ autotest/apitest/apiauto_testcase3.py
@@ -11,8 +11,6 @@ from autotest.common import mySqlOperation
 
 import pymysql
 
-
-
 HOSTNAME = "127.0.0.1"
 
 def readSQLcase():                     # 读取数据库中相应的接口用例数据
@@ -30,6 +28,37 @@ def interfaceTest(case_list):
     request_urls = []
     responses = []
     strinfo = re.compile('{TaskId}')
+    strinfo1 = re.compile('{AssetId}')
+    strinfo2 = re.compile('{PointId}')
+    assetinfo = re.compile('{assetno}')
+    tasknoinfo = re.compile('{tasknoinfo}')
+    schemainfo = re.compile('{schema}')
+    for case in case_list:
+        try:
+            case_id = case[0]
+            interface_name = case[1]
+            url = case[2]
+            method = case[3]
+            param = case[4]
+            res_check = case[5]
+        except Exception as e:
+            return "测试用例格式不正确%s"%e  
+
+        if param == "":
+            new_url = "http://"+"api.test.com.cn"+url
+        elif param == "null":
+            new_url = "http://"+url
+        else:
+            # 正则表达式匹配
+            url = strinfo.sub(TaskId,url)  
+            param= strinfo.sub(TaskId,param)
+            param = tasknoinfo.sub(taskno,param)
+            new_url = "http://"+"127.0.0.1"+url
+            request_urls.append(new_url)
+        if method.upper() == "GET":
+            headers = {"Authorization":"","Content-Type":"application/json"}       
+            if "=" in urlParam(param):   
+
 
 
 
